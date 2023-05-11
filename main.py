6b827bf5b6e91347b32a5bd8475e7d15ee86ad6c
c1007303 <b.k.kirkwood2@newcastle.ac.uk>
2023-05-11 14:36:05 +0100

    First implementation of derive()

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 # optional programming exercise for fundamentals, week 6
 
-def derive():
-    pass
+def derive(coeffs):
+    # coeffs is dictionary as {order of x: coefficient}
+    results = {}
+    for orders in coeffs:
+        results[orders - 1] = orders * coeffs[orders]
+    return results
+
 
 def newton():
     pass
+
+
+def main():
+    highest = int(input("Enter highest order: "))
+    lowest = int(input("Enter lowest order: "))
+    c = {}
+    for i in range(highest, lowest - 1, -1):
+        c[i] = int(input(f"Enter coefficient of x^{i}."))
+    derivative = derive(c)
+    print("Result:")
+    result = []
+    for i in derivative:
+        result.append(f"{derivative[i]}x^{i}")
+    print(" + ".join(result))
+
+main()
