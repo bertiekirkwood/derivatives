e9261e9833fc042814c8508e074c54295d6379db
c1007303 <b.k.kirkwood2@newcastle.ac.uk>
2023-05-11 15:13:23 +0100

    Added nth derivative

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 # optional programming exercise for fundamentals, week 6
 
-def derive(coeffs):
+def derive(coefficients, n):
+    """Calculates nth derivatives of polynomial stored in coefficients."""
     # coeffs is dictionary as {order of x: coefficient}
-    results = {}
-    for orders in coeffs:
-        results[orders - 1] = orders * coeffs[orders]
-    return results
+    if n == 0:
+        return coefficients
+    else:
+        results = {}
+        for orders in coefficients:
+            results[orders - 1] = orders * coefficients[orders]
+        return derive(results, n-1)
+
+
+def derivative_to_string(coefficients):
+    # takes dict format of derivative and outputs string
+    elements = []
+    for i in coefficients:
+        if coefficients[i] != 0:
+            if coefficients[i] == 1 and i != 0:
+                elements.append(f"x^{i}")
+            else:
+                if i == 0:
+                    elements.append(str(coefficients[i]))
+                elif i == 1:
+                    elements.append(f"{coefficients[i]}x")
+                else:
+                    elements.append(f"{coefficients[i]}x^{i}")
+
+    output_string = ""
+    for index, x in enumerate(elements):
+        if x[0] == '-':
+            output_string += x[1:]
+        else:
+            output_string += x
+        if index < len(elements) - 1:
+            if elements[index+1][0] == '-':
+                output_string += " - "
+            else:
+                output_string += " + "
+    return output_string
 
 
 def newton():
@@ -13,16 +46,16 @@ def newton():
 
 
 def main():
+    degree = int(input("Enter degree of derivative: "))
     highest = int(input("Enter highest order: "))
     lowest = int(input("Enter lowest order: "))
     c = {}
     for i in range(highest, lowest - 1, -1):
-        c[i] = int(input(f"Enter coefficient of x^{i}."))
-    derivative = derive(c)
-    print("Result:")
-    result = []
-    for i in derivative:
-        result.append(f"{derivative[i]}x^{i}")
-    print(" + ".join(result))
+        if i != 0:
+            c[i] = int(input(f"Enter coefficient of x^{i}: "))
+        else:
+            c[i] = int(input("Enter constant: "))
+    print("Result: " + derivative_to_string(derive(c, degree)))
+
 
 main()
